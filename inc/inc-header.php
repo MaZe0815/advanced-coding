1418ca942bae4bb9256fadcd969de725da115148
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-11-09 16:44:38 +0100

    renaming overall article page and linking

--- inc/inc-header.php
+++ inc/inc-header.php
@@ -1,7 +1,7 @@
 <div class="header ac_card">
     <div class="topnav">
         <a href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>" class="topnav-icons fa fa-home ac-left ac-bar-item ac-button active" title="Home"></a>
-        <a class="ac-bar-item ac-button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikelubersicht">Artikel&uuml;bersicht</a>
+        <a class="ac-bar-item ac-button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht">Artikel&uuml;bersicht</a>
         <a class="ac-bar-item ac-button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/kontakt">Kontakt</a>
         <a class="ac-bar-item ac-button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/impressum">Impressum</a>
         <a class="topnav-icons fa fa-search ac-right ac-bar-item ac-button"  id="search_click" onclick="display_function('inline_search', this.id);"></a>
