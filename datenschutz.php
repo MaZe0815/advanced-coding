0d5633d33c2aa1eb0bedd42345d25d483d59243f
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-11-09 17:18:20 +0100

    Filling dataprotection with example contents

--- datenschutz.php
+++ datenschutz.php
@@ -5,7 +5,7 @@
         <?php include 'inc/inc-meta.php'; ?>
         <?php
         $set_meta_tags = new set_meta_tags();
-        $set_meta_tags->seo_array['title'] = "Datenschutz - Shop";
+        $set_meta_tags->seo_array['title'] = "Datenschutzerkl&auml;rung - Shop";
         $set_meta_tags->seo_array['description'] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.";
         $set_meta_tags->seo_array['keywords'] = "Lorem, ipsum, dolor, sit, amet, consectetuer, adipiscin, elit, Aenean commodo";
         $set_meta_tags->seo_array['index_allow'] = false;
@@ -20,10 +20,194 @@
                     <img src="https://via.placeholder.com/1920x450" alt="Startseite">
                 </div>
                 <?php include 'inc/inc-menu.php'; ?>
-                <h1>Datenschutz</h1>
+                <h1>Datenschutzerkl&auml;rung</h1>
+                <h2>Grundlegendes</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                    <br><a class="button" href="#">Button</a>
+                    Diese Datenschutzerkl&auml;rung soll die Nutz
+                    er dieser Website &uuml;ber die Art, den Umfang und
+                    den Zweck der Erhebung und Verwendung personenbezogener Daten durch den
+                    Websitebetreiber [Ihre Kontaktdaten einf&uuml;gen] informieren.
+                    Der Websitebetreiber nimmt Ihren Datenschutz sehr ernst und behandelt Ihre
+                    pe
+                    rsonenbezogenen Daten vertraulich und entsprechend der gesetzlichen Vorschriften. Da
+                    durch neue Technologien und die st&auml;ndige Weiterentwicklung dieser Webseite &Auml;nderungen
+                    an dieser Datenschutzerkl&auml;rung vorgenommen werden k&ouml;nnen, empfehlen wir Ihnen sich di
+                    e
+                    Datenschutzerkl&auml;rung in regelm&auml;&szlig;igen Abst&auml;nden wieder durchzulesen.
+                    Definitionen der verwendeten Begriffe (z.B. &ldquo;personenbezogene Daten&rdquo; oder &ldquo;Verarbeitung&rdquo;)
+                    finden Sie in Art. 4 DSGVO
+                </p>
+                <hr>
+                <h2>Zugriffsdaten</h2>
+                <p>
+                    Wir, der Websitebetreiber bzw. Seitenprovider, erhe
+                    ben aufgrund unseres berechtigten
+                    Interesses (s. Art. 6 Abs. 1 lit. f. DSGVO) Daten &uuml;ber Zugriffe auf die Website und speichern
+                    diese als &bdquo;Server
+                    -
+                    Logfiles&ldquo; auf dem Server der Website ab. Folgende Daten werden so
+                    protokolliert:
+                </p>
+                <ul>
+                    <li>Besuchte Website</li>
+                    <li>Uhrzeit zum Zeitpunkt des Zugriffes</li>
+                    <li>Menge der gesendeten Daten in Byte</li>
+                    <li>Quelle/Verweis, von welchem Sie auf die Seite gelangten</li>
+                    <li>Verwendeter Browser</li>
+                    <li>Verwendetes Betriebssystem</li>
+                    <li>Verwendete IP-Adresse</li>
+                </ul>
+                <p>
+                    Die Server
+                    -
+                    Logfiles werden f&uuml;r maximal 7 Tage gespeichert und anschlie
+                    &szlig;end gel&ouml;scht. Die
+                    Speicherung der Daten erfolgt aus Sicherheitsgr&uuml;nden, um z. B. Missbrauchsf&auml;lle aufkl&auml;ren
+                    zu k&ouml;nnen. M&uuml;ssen Daten aus Beweisgr&uuml;nden aufgehoben werden, sind sie solange von der
+                    L&ouml;schung ausgenommen bis der Vorfall endg&uuml;ltig gekl&auml;rt ist.
+                </p>
+                <hr>
+                <h2>Reichweitenmessung & Cookies</h2>
+                <p>
+                    Diese Website verwendet Cookies zur pseudonymisierten Reichweitenmessung, die
+                    entweder von unserem Server oder dem Server Dritter an den Browser des Nutzers
+                    Muster von
+                    datenschutz.org
+                    &uuml;bertragen werden. Bei Cookies handelt es sich um kleine Dateien, wel
+                    che auf Ihrem
+                    Endger&auml;t gespeichert werden. Ihr Browser greift auf diese Dateien zu. Durch den Einsatz
+                    von Cookies erh&ouml;ht sich die Benutzerfreundlichkeit und Sicherheit dieser Website.
+                    Falls Sie nicht m&ouml;chten, dass Cookies zur Reichweitenmessung auf Ihrem E
+                    ndger&auml;t
+                    gespeichert werden, k&ouml;nnen Sie dem Einsatz dieser Dateien hier widersprechen:
+                </p>
+                <ul>
+                    <li>Cookie-Deaktivierungsseite der Netzwerkwerbeinitiative: <a href="http://optout.networkadvertising.org/?c=1#!/" target="_blank">http://optout.networkadvertising.org/?c=1#!/</a></li>
+                    <li>Co okie-Deaktivierungsseite der US-amerikanischen Website: <a href="http://optout.aboutads.info/?c=2#!/">http://optout.aboutads.info/?c=2#!/</a></li>
+                    <li>Cookie-Deaktivierungsseite der europ&auml;ischen Website: <a href="http://optout.networkadvertising.org/?c=1#!/">http://optout.networkadvertising.org/?c=1#!/</a></li>
+                </ul>
+                <p>
+                    G&auml;ngige Browser bieten die Einstellungsoption, Cookies nicht zuzulassen. Hinweis: Es ist
+                    nicht gew&auml;hrleistet, dass Sie auf alle Funktionen dieser Website ohne Einschr&auml;nkungen
+                    zugreifen k&ouml;nnen, wenn
+                    Sie entsprechende Einstellungen vornehmen.
+                    Erfassung und Verarbeitung personenbezogener Daten
+                    Der Websitebetreiber erhebt, nutzt und gibt Ihre personenbezogenen Daten nur dann weiter,
+                    wenn dies im gesetzlichen Rahmen erlaubt ist oder Sie in die Datenerhe
+                    bung einwilligen.
+                    Als personenbezogene Daten gelten s&auml;mtliche Informationen, welche dazu dienen, Ihre
+                    Person zu bestimmen und welche zu Ihnen zur&uuml;ckverfolgt werden k&ouml;nnen
+                    &ndash;
+                    also
+                    beispielsweise Ihr Name, Ihre E
+                    -
+                    Mail
+                    -
+                    Adresse und Telefonnummer.
+                    Diese Website
+                    k&ouml;nnen Sie auch besuchen, ohne Angaben zu Ihrer Person zu machen. Zur
+                    Verbesserung unseres Online
+                    -
+                    Angebotes speichern wir jedoch (ohne Personenbezug) Ihre
+                    Zugriffsdaten auf diese Website. Zu diesen Zugriffsdaten geh&ouml;ren z. B. die von Ihnen
+                    angeforderte Da
+                    tei oder der Name Ihres Internet
+                    -
+                    Providers. Durch die Anonymisierung der
+                    Daten sind R&uuml;ckschl&uuml;sse auf Ihre Person nicht m&ouml;glich.
+                </p>
+                <hr>
+                <h2>Umgang mit Kontaktdaten</h2>
+                <p>
+                    Nehmen Sie mit uns als Websitebetreiber durch die angebotenen Kontaktm&ouml;glichkeiten
+                    Verbindung auf, werden Ihre Angaben gespeichert, damit auf diese zur Bearbeitung und
+                    Beantwo
+                    rtung Ihrer Anfrage zur&uuml;ckgegriffen werden kann. Ohne Ihre Einwilligung werden
+                    diese Daten nicht an Dritte weitergegeben.
+                </p>
+                <hr>
+                <h2>Umgang mit Kommentaren und Beitr&auml;gen</h2>
+                <p>
+                    Hinterlassen Sie auf dieser Website einen Beitrag oder Kommentar, wird Ihre IP
+                    -
+                    Adresse
+                    gespeich
+                    ert. Dies erfolgt aufgrund unserer berechtigten Interessen im Sinne des Art. 6 Abs.
+                    1 lit. f. DSGVO und dient der Sicherheit von uns als Websitebetreiber: Denn sollte Ihr
+                    Kommentar gegen geltendes Recht versto&szlig;en, k&ouml;nnen wir daf&uuml;r belangt werden, weshalb
+                    w
+                    ir ein Interesse an der Identit&auml;t des Kommentar
+                    -
+                    bzw. Beitragsautors haben.
+                </p>
+                <hr>
+                <h2>Newsletter-Abonnement</h2>
+                <p>
+                    Der Websitebetreiber bietet Ihnen einen Newsletter an, in welchem er Sie &uuml;ber aktuelle
+                    Geschehnisse und Angebote informiert. M&ouml;chten Sie den Newsletter abonnieren, m&uuml;sse
+                    n
+                    Sie eine valide E
+                    -
+                    Mail
+                    -
+                    Adresse angeben. Wenn Sie den Newsletter abonnieren, erkl&auml;ren Sie
+                    sich mit dem Newsletter
+                    -
+                    Empfang und den erl&auml;uterten Verfahren einverstanden.
+                    Der Newsletterversand erfolgt durch den Versanddienstleister XY, einer Versandplattform
+                    des Anbieters ABC, Beispielstra&szlig;e 5, 12345 Beispielstadt, Deutschland. Informationen &uuml;ber
+                    die Datenschutzbestimmungen des Versanddienstleisters erhalten Sie unter: beispiel
+                    -
+                    XY.de/datenschutz.
+                    Widerruf und K&uuml;ndigung: Ihre Einwilligung zum Erhalt des Newsl
+                    etter k&ouml;nnen Sie jederzeit
+                    widerrufen und somit das Newsletter
+                    -
+                    Abonnement k&uuml;ndigen. Nach Ihrer K&uuml;ndigung erfolgt
+                    die L&ouml;schung Ihre personenbezogenen Daten. Ihre Einwilligung in den Newsletterversand
+                    erlischt gleichzeitig. Am Ende jedes Newsletters finden S
+                    ie den Link zur K&uuml;ndigung.
+                </p>
+                <hr>
+                <h2>Rechte des Nutzers</h2>
+                <p>
+                    Sie haben als Nutzer das Recht, auf Antrag eine kostenlose Auskunft dar&uuml;ber zu erhalten,
+                    welche
+                    personenbezogenen Daten &uuml;ber Sie gespeichert wurden. Sie haben au&szlig;erdem das
+                    Recht auf Berichtigung falscher Daten und auf die Verarbeitungseinschr&auml;nkung oder
+                    L&ouml;schung Ihrer personenbezogenen Daten. Falls zutreffend, k&ouml;nnen Sie auch Ihr Recht auf
+                    Datenport
+                    abilit&auml;t geltend machen. Sollten Sie annehmen, dass Ihre Daten unrechtm&auml;&szlig;ig
+                    verarbeitet wurden, k&ouml;nnen Sie eine Beschwerde bei der zust&auml;ndigen Aufsichtsbeh&ouml;rde
+                    einreichen.
+                </p>
+                <h3>Widerspruchsrecht</h3>
+                <p>
+                    Sofern Ihr Wunsch nicht mit einer gesetzlichen Pflicht zur Aufb
+                    ewahrung von Daten (z. B.
+                    Vorratsdatenspeicherung) kollidiert, haben Sie ein Anrecht auf L&ouml;schung Ihrer Daten. Von
+                    uns gespeicherte Daten werden, sollten sie f&uuml;r ihre Zweckbestimmung nicht mehr vonn&ouml;ten
+                    sein und es keine gesetzlichen Aufbewahrungsfristen g
+                    eben, gel&ouml;scht. Falls eine L&ouml;schung
+                    nicht durchgef&uuml;hrt werden kann, da die Daten f&uuml;r zul&auml;ssige gesetzliche Zwecke erforderlich
+                    sind, erfolgt eine Einschr&auml;nkung der Datenverarbeitung. In diesem Fall werden die Daten
+                    gesperrt und nicht f&uuml;r andere Zwecke vera
+                    rbeitet.
+                </p>
+                <h3>L&ouml;schung von Daten</h3>
+                <p>
+                    Nutzer dieser Webseite k&ouml;nnen von ihrem Widerspruchsrecht Gebrauch machen und der
+                    Verarbeitung ihrer personenbezogenen Daten zu jeder Zeit widersprechen.
+
+                    Wenn Sie eine Berichtigung, Sperrung, L&ouml;schung oder Auskunft &uuml;ber die z
+                    u Ihrer Person
+                    gespeicherten personenbezogenen Daten w&uuml;nschen oder Fragen bzgl. der Erhebung,
+                    Verarbeitung oder Verwendung Ihrer personenbezogenen Daten haben oder erteilte
+                    Einwilligungen widerrufen m&ouml;chten, wenden Sie sich bitte an folgende E
+                    -
+                    Mail
+                    -
+                    Adresse: [shop@mustershop.biz]
                 </p>
             </div>
             <?php include 'inc/inc-footer.php'; ?>
