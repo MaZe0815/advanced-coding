4779cef843fa62c34645fab88e9fdf37f05a0d9b
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-11-23 12:46:59 +0100

    Merged header into grid system

--- inc/inc-header.php
+++ inc/inc-header.php
@@ -1,12 +1,18 @@
-<div class="header ac_card">
-    <div class="topnav">
-        <a href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>" class="topnav-icons fa fa-home ac-left ac-bar-item ac-button <?php echo (SCRIPT_NAME === "index.php" ? 'active' : ''); ?>" title="Home"></a>
-        <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "artikeluebersicht.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht">Artikel&uuml;bersicht</a>
-        <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "kontakt.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/kontakt">Kontakt</a>
-        <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "impressum.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/impressum">Impressum</a>
-        <a class="topnav-icons fa fa-search ac-right ac-bar-item ac-button"  id="search_click" onclick="display_function('inline_search', this.id);"></a>
-        <a class="topnav-icons fa fa-shopping-cart ac-right ac-bar-item ac-button" id="cart_click" onclick="display_function('inline_search', this.id);"></a>
-        <a class="ac-bar-item ac-button ac-right <?php echo (SCRIPT_NAME === "login.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/login">Login</a>
-        <a class="ac-bar-item ac-button ac-right <?php echo (SCRIPT_NAME === "registrieren.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/registrieren">Registrieren</a>
+<div class="container">
+    <div class="row">
+        <div class="header ac_card">
+            <div class="topnav">
+                <div class="twelve columns">
+                    <a class="topnav-icons fa fa-home ac-left ac-bar-item ac-button <?php echo (SCRIPT_NAME === "index.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>"></a>
+                    <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "artikeluebersicht.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht">Artikel&uuml;bersicht</a>
+                    <div class="ac-right">
+                        <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "login.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/login">Login</a>
+                        <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "registrieren.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/registrieren">Registrieren</a>
+                        <a class="topnav-icons fa fa-search ac-bar-item ac-button" id="search_click" onclick="display_function('inline_search', this.id);"></a>
+                        <a class="topnav-icons fa fa-shopping-cart ac-bar-item ac-button" id="cart_click" onclick="display_function('inline_search', this.id);"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
