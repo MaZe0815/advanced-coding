073497196ba1ed175a952824cc18e4072dfce5c5
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-11-23 16:17:40 +0100

    Added web font type "Montserrat" via google cdn

--- inc/inc-meta.php
+++ inc/inc-meta.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link rel="shortcut icon" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/img/favicon.ico">
-<link rel="stylesheet" type="text/css" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/css/font-awesome.min.css" media="screen">
+<link rel="stylesheet" type="text/css" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"  media="screen">
 <link rel="stylesheet" type="text/css" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/css/normalize.min.css" media="screen">
 <link rel="stylesheet" type="text/css" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/css/styles.css" media="screen">
