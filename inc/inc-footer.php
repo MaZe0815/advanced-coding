a8a553b5d18375c16ff6921be2328dfe40fee4c3
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-11-23 11:32:53 +0100

    Merged footer include into grid system

--- inc/inc-footer.php
+++ inc/inc-footer.php
@@ -1,5 +1,14 @@
-<div class="footer ac_card">
-    <a class="ac-bar-item ac-button ac-left" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>">&copy; Copyright 2018 - All rights reserved</a>
-    <a class="ac-bar-item ac-button ac-right" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/impressum">Impressum</a>
-    <a class="ac-bar-item ac-button ac-right" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/datenschutz">Datenschutz</a>
+<div class="container">
+    <div class="row">
+        <div class="footer ac_card">
+            <div class="six columns">
+                <a class="ac-bar-item ac-button ac-left" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>">&copy; Copyright 2018 - All rights reserved</a>
+            </div>
+            <div class="six columns">
+                <a class="ac-bar-item ac-button ac-right" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/impressum">Impressum</a>
+                <a class="ac-bar-item ac-button ac-right" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/datenschutz">Datenschutz</a>
+                <a class="ac-bar-item ac-button ac-right" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/agb">AGB</a>
+            </div>
+        </div>
+    </div>
 </div>
