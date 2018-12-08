3f0b11ceae5dced8dff0a8e5cd016fe7d2ea17cf
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-12-08 19:25:34 +0100

    Optimized Session handling for user credentials

--- inc/inc-header.php
+++ inc/inc-header.php
@@ -11,8 +11,14 @@
                     </div>
                     <div class="col-6">
                         <div class="ac-right">
-                            <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "login.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/login">Login</a>
-                            <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "registrieren.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/registrieren">Registrieren</a>
+                            <?php if (isset($_SESSION['user'])) { ?>
+                                <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "logout.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/logout">Logout</a>
+                            <?php } else { ?>
+                                <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "login.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/login">Login</a>
+                            <?php } ?>
+                            <?php if (!isset($_SESSION['user'])) { ?>
+                                <a class="ac-bar-item ac-button <?php echo (SCRIPT_NAME === "registrieren.php" ? 'active' : ''); ?>" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/registrieren">Registrieren</a>
+                            <?php } ?>
                             <input type="text" placeholder="Ihr Suchbegriff..." id="input_inline_search" onkeyup="asyn_search(csrfObject.baseURL + '/ajax/async_search/', 'inline_search', this.value);">
                             <a class="topnav-icons fa fa-shopping-cart ac-bar-item ac-button" id="cart_click" onclick="display_function('inline_cart', this.id);"></a>
                         </div>
