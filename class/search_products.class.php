b2a1d5f001e14475b9cde4df421fbf5a4fa4b0ef
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-12-30 23:40:25 +0100

    Added quantity optimizations for inline search and single item query

--- class/search_products.class.php
+++ class/search_products.class.php
@@ -5,6 +5,7 @@ class search_products extends get_products {
     public $conn;
     public $search_str;
     public $search_str_json = true;
+    private $quantity_up_to = 6;
 
     function __construct() {
 
@@ -27,7 +28,7 @@ class search_products extends get_products {
 
         if (strlen($this->conn->connect_error) === 0) {
 
-            $sql_product = "SELECT id, product_name, price, pid, gid FROM acs_products WHERE MATCH(product_name) AGAINST('" . $this->search_str . "*' IN BOOLEAN MODE) ORDER BY MATCH(product_name) AGAINST('" . $this->search_str . "*' IN BOOLEAN MODE)  LIMIT " . $this->product_limit;
+            $sql_product = "SELECT id, product_name, price, pid, gid FROM acs_products WHERE quantity > " . $this->quantity_up_to . " AND MATCH(product_name) AGAINST('" . $this->search_str . "*' IN BOOLEAN MODE) ORDER BY MATCH(product_name) AGAINST('" . $this->search_str . "*' IN BOOLEAN MODE)  LIMIT " . $this->product_limit;
             $result_product = $this->conn->query($sql_product);
 
             if ($result_product->num_rows === 1) {
