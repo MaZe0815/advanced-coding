8f2bf02efe37b9e1df8148dac27d78970443dfa7
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-12-31 00:57:43 +0100

    Added dynamic output of copyright year

--- inc/inc-footer.php
+++ inc/inc-footer.php
@@ -3,7 +3,7 @@
         <div class="footer ac_card">
             <div class="row">
                 <div class="col-6">
-                    <a class="ac-bar-item ac-button ac-left" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>">&copy; Copyright 2018 - All rights reserved</a>
+                    <a class="ac-bar-item ac-button ac-left" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>">&copy; Copyright <?php echo date('Y'); ?> - All rights reserved</a>
                 </div>
                 <div class="col-6">
                     <a class="ac-bar-item ac-button ac-right" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/impressum">Impressum</a>
