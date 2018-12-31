057e062ba37ecf8b842e505d86ff9ff3356e17f4
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-12-31 17:23:04 +0100

    Removed row from markup of pagination

--- inc/inc-pagination.php
+++ inc/inc-pagination.php
@@ -1,37 +1,35 @@
-<div class="row">
-    <div class="col-12">
-        <ul class="pagination">
-            <?php if ($get_products->curpage != $get_products->startpage) { ?>
-                <li class="page-item ">
-                    <a class="button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->startpage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self">
-                        <i class="fa fa-arrow-left"></i>
-                    </a>
-                </li>
-            <?php } ?>
-            <?php if ($get_products->curpage >= 2) { ?>
-                <li class="page-item">
-                    <a class="button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->previouspage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self"><?php echo $get_products->previouspage; ?></a>
-                </li>
-            <?php } ?>
-            <li class="page-item">
-                <a class="button button-primary" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->curpage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self"><?php echo $get_products->curpage; ?></a>
+<div class="col-12">
+    <ul class="pagination">
+        <?php if ($get_products->curpage != $get_products->startpage) { ?>
+            <li class="page-item ">
+                <a class="button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->startpage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self">
+                    <i class="fa fa-arrow-left"></i>
+                </a>
             </li>
-            <?php if ($get_products->curpage != $get_products->endpage) { ?>
-                <li class="page-item ">
-                    <a class="button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->nextpage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self"><?php echo $get_products->nextpage; ?></a>
-                </li>
-            <?php } ?>
-            <?php if ($get_products->curpage != $get_products->endpage) { ?>
-                <li class="page-item">
-                    <a class="button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->endpage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self">
-                        <i class="fa fa-arrow-right"></i>
-                    </a>
-                </li>
-            <?php } ?>
-        </ul>
-        <p class="pagination_totals">
-            Seite <span><?php echo $get_products->curpage; ?></span> von <span><?php echo $get_products->endpage; ?></span> Seite<?php if ($get_products->endpage > 1) { ?>n<?php } ?>.
-            Gesamtergebnisse: <span><?php echo $get_products->total_res; ?></span> Artikel.
-        </p>
-    </div>
+        <?php } ?>
+        <?php if ($get_products->curpage >= 2) { ?>
+            <li class="page-item">
+                <a class="button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->previouspage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self"><?php echo $get_products->previouspage; ?></a>
+            </li>
+        <?php } ?>
+        <li class="page-item">
+            <a class="button button-primary" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->curpage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self"><?php echo $get_products->curpage; ?></a>
+        </li>
+        <?php if ($get_products->curpage != $get_products->endpage) { ?>
+            <li class="page-item ">
+                <a class="button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->nextpage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self"><?php echo $get_products->nextpage; ?></a>
+            </li>
+        <?php } ?>
+        <?php if ($get_products->curpage != $get_products->endpage) { ?>
+            <li class="page-item">
+                <a class="button" href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME; ?>/artikeluebersicht?p=<?php echo $get_products->endpage . $get_products->filter_pagination_adds; ?>#product-listing" target="_self">
+                    <i class="fa fa-arrow-right"></i>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+    <p class="pagination_totals">
+        Seite <span><?php echo $get_products->curpage; ?></span> von <span><?php echo $get_products->endpage; ?></span> Seite<?php if ($get_products->endpage > 1) { ?>n<?php } ?>.
+        Gesamtergebnisse: <span><?php echo $get_products->total_res; ?></span> Artikel.
+    </p>
 </div>
