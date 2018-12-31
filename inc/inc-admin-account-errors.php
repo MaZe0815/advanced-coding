f37f4fbd134d7edb2955b32b1c134d6cf42da9d0
Mark Zembrzycki <markzembrzycki@gmx.de>
2018-12-31 17:28:25 +0100

    Added optimizations for administration error includes

--- inc/inc-admin-account-errors.php
+++ inc/inc-admin-account-errors.php
@@ -5,7 +5,7 @@
     </div>
 <?php } elseif (is_array($administraction_accounts->customer_data_error) && count($administraction_accounts->customer_data_error) > 0) { ?>
     <div class="error" id="notification">
-        Leider ist ein Fehler aufgetreten, bitte pr&uuml;fen Sie nochmals Ihre Eingaben auf Vollst&auml;ndigkeit!
+        Leider ist ein Fehler aufgetreten, bitte pr&uuml;fen Sie nochmals Ihre Eingaben auf Vollst&auml;ndigkeit!<br>
         <a href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME . '/administration-konten' ?>" target="_self">Zur&uuml;ck</a> zu den Benutzerkonten.
     </div>
 <?php } ?>
--- inc/inc-usererrors.php
+++ inc/inc-usererrors.php
@@ -1,9 +1,11 @@
 <?php if (isset($_GET['s']) && strlen($_GET['s'])) { ?>
     <div class="success" id="notification">
-        Ihre Stammdaten wurden erfolgreich aktualisiert. Bitte beachten Sie dass <strong style="font-weight: 600;">Ihre Bestellung</strong> ab sofort an die von Ihnen <strong style="font-weight: 600;">neu angegebene Versandadresse</strong> Ihres Kundenkontos <strong style="font-weight: 600;">versendet</strong> werden!
+        Ihre Stammdaten wurden erfolgreich aktualisiert. Beachten Sie dass <strong style="font-weight: 600;">Ihre Bestellung</strong> ab sofort an die von Ihnen <strong style="font-weight: 600;">neu angegebene Versandadresse</strong> Ihres Kundenkontos <strong style="font-weight: 600;">versendet</strong> werden!<br>
+        <a href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME . '/kundenkonto' ?>" target="_self">Zur&uuml;ck</a> zum Kundenkonto.
     </div>
 <?php } elseif (is_array($customer_account->customer_data_error) && count($customer_account->customer_data_error) > 0) { ?>
     <div class="error" id="notification">
-        Leider ist ein Fehler aufgetreten, bitte pr&uuml;fen Sie nochmals Ihre Eingaben auf Vollständigkeit!
+        Leider ist ein Fehler aufgetreten, bitte pr&uuml;fen Sie nochmals Ihre Eingaben auf Vollst&auml;ndigkeit!<br>
+        <a href="<?php echo HTTP_HOST . ROOT_URL . PROJECT_NAME . '/kundenkonto' ?>" target="_self">Zur&uuml;ck</a> zum Kundenkonto.
     </div>
 <?php } ?>
